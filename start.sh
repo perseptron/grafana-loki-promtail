cf3f60f5cfb89726c585b6534316ba90f79e62c4
mandrake <perseptron@gmail.com>
2023-08-24 20:12:11 +0000

    small fixes

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 go get webapp &&
-
-go run /goapp/main.go 2 >> /var/log/apka.log &&
-
-/usr/local/bin/promtail-linux-amd64 -config.file /etc/promtail.yaml 
+go run /goapp/main.go 2 >> /var/log/apka.log &
+/usr/local/bin/promtail-linux-amd64 -config.file /etc/promtail.yaml
