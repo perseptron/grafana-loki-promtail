14e19be45f48b6c2cb7d3a68245860a899186a25
mandrake <perseptron@gmail.com>
2023-08-24 17:57:49 +0000

    logging functionality added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "github.com/labstack/echo/v4"
 	"webapp/pkg/config"
+    "github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -14,5 +15,6 @@ func main() {
     e.GET("/", func(c echo.Context) error {
         return c.File("public/views/webapp.html")
     })
+    e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{}))
     e.Start(":" + port)
 }
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-/usr/local/bin/promtail-linux-amd64 -config.file /etc/promtail.yaml &
+go get webapp &&
 
-go run /goapp/main.go
+go run /goapp/main.go 2 >> /var/log/apka.log &&
+
+/usr/local/bin/promtail-linux-amd64 -config.file /etc/promtail.yaml 
